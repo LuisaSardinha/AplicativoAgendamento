1bcb32f71da6fe7854f29b6f59aa8a56e7a30c3e
Luisa Sardinha <sardinhaluisa06@gmail.com>
2025-05-17 23:40:45 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
